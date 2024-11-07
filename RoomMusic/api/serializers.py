15021f6714ccff71a78c6e51361c7374cf84ca54
Timelaying <makanjuola.timi@gmail.com>
2024-11-07 21:11:09 +0000

    meta for models

--- RoomMusic/api/serializers.py
+++ RoomMusic/api/serializers.py
@@ -1,2 +1,8 @@
 #a serializer turns the model and informatioin to JSON, i.e also tracking for changes in models
 from rest_framework import serializers
+from .models import Room
+
+class RoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('id', 'code', 'host', 'guest_can_pause', 'votes_to_skip')
