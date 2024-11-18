d873f84236146b87fc62328dc8cd31af3914e0f3
Timelaying <makanjuola.timi@gmail.com>
2024-11-18 20:59:31 +0000

    create room page

--- RoomMusic/frontend/src/components/CreateRoomPage.js
+++ RoomMusic/frontend/src/components/CreateRoomPage.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 
-export default class RoomJoinPage extends Component {
+export default class CreateRoomPage extends Component {
     construcor(props) {
         super(props);
     }
 
     render(){
-        return <p>This is the page to join</p>;
+        return <p>This is the page to create a room</p>;
     }
 }
