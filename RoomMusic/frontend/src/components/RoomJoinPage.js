7ad507e64e29ffb63463d10e51eaa6b45e753f5b
Timelaying <makanjuola.timi@gmail.com>
2024-11-18 20:57:08 +0000

    roomjoin

---
+++ RoomMusic/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class RoomJoinPage extends Component {
+    construcor(props) {
+        super(props);
+    }
+
+    render(){
+        return <p>This is the home page</p>;
+    }
+}
