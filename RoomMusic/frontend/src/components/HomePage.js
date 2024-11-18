50456bd1473fca4a8e7d184bb67b719b02464d06
Timelaying <makanjuola.timi@gmail.com>
2024-11-18 20:51:23 +0000

    Hompage Component

---
+++ RoomMusic/frontend/src/components/HomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class HomePage extends Component {
+    construcor(props) {
+        super(props);
+    }
+
+    render(){
+        return <p>This is the home page</p>;
+    }
+}
