e841deae3d28d9f7e57a635dd7590a54ff907e3c
Timelaying <makanjuola.timi@gmail.com>
2024-11-19 10:48:58 +0000

    formattng

--- RoomMusic/frontend/src/components/HomePage.js
+++ RoomMusic/frontend/src/components/HomePage.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import RoomJoinPage from "./RoomJoinPage";
 import CreateRoomPage from "./CreateRoomPage";
-import { BrowserRouter as Router, Switch, Route, Link, Redirect, } from "react-router-dom"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
 
 export default class HomePage extends Component {
-    construcor(props) {
-        super(props);
-    }
+  construcor(props) {
+    super(props);
+  }
 
-    render(){
-        return <p>This is the home page</p>;
-    }
+  render() {
+    return <p>This is the home page</p>;
+  }
 }
