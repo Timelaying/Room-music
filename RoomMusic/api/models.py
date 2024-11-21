6abd2f3acb7ec9677a62f63f0b68d1d3c4ab0dd0
Timelaying <makanjuola.timi@gmail.com>
2024-11-21 19:25:12 +0000

    generate unique code

--- RoomMusic/api/models.py
+++ RoomMusic/api/models.py
@@ -15,7 +15,7 @@ def generate_unique_code():
 
 # Create your models here.
 class Room(models.Model):
-    code = models.CharField(max_length=8, default="", unique=True) # code to access the room
+    code = models.CharField(max_length=8, default= generate_unique_code, unique=True) # code to access the room
     host = models.CharField(max_length=8, unique=True)
     guest_can_pause = models.BooleanField(null=False, default=False)
     votes_to_skip = models.IntegerField(null=False, default=1)
