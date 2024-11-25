ccdf54f0c98e5401d3cb9afd792824e36c313faf
Timelaying <makanjuola.timi@gmail.com>
2024-11-25 21:27:00 +0000

    re code

--- RoomMusic/api/views.py
+++ RoomMusic/api/views.py
@@ -5,13 +5,17 @@ from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+
 # Create your views here.
-class RoomView(generics.CreateAPIView):
+
+
+class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
 
+
 class CreateRoomView(APIView):
-    serializer_class =CreateRoomSerializer
+    serializer_class = CreateRoomSerializer
 
     def post(self, request, format=None):
         if not self.request.session.exists(self.request.session.session_key):
@@ -24,12 +28,15 @@ class CreateRoomView(APIView):
             host = self.request.session.session_key
             queryset = Room.objects.filter(host=host)
             if queryset.exists():
-                room = queryset[0] #taking the first result annd th below are updating it
+                room = queryset[0]
                 room.guest_can_pause = guest_can_pause
                 room.votes_to_skip = votes_to_skip
-                room.save(update_fields=['guest_can_pause','votes_to_skip'])
+                room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+                return Response(RoomSerializer(room).data, status=status.HTTP_200_OK)
             else:
-                room = Room(host=host, guest_can_pause=guest_can_pause, votes_to_skip=votes_to_skip)
+                room = Room(host=host, guest_can_pause=guest_can_pause,
+                            votes_to_skip=votes_to_skip)
                 room.save()
-            return Response(RoomSerializer(room).data, status=status.HTTP_)
-            
+                return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
+
+        return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
