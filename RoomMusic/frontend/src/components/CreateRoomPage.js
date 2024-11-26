fd36602b90ca8d10d91e567cff4fc83781c6a8ff
Timelaying <makanjuola.timi@gmail.com>
2024-11-26 20:21:48 +0000

    import

--- RoomMusic/frontend/src/components/CreateRoomPage.js
+++ RoomMusic/frontend/src/components/CreateRoomPage.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import Button from "@material-ui/core/Button";
-import Grid from "@material-ui/core/Grid"
-import Typography from "@material-ui/core/Typography"
-import Textfield from "@material-ui/core/Textfield"
-import FormHelperText from "@material-ui/core/FormHelperText"
-import FormControl from "@material-ui/core/FormControl"
-import { Link } from "react-router-dom"
-import Radio from "@material-ui/core/Radio"
-import RadioGroup from "@material-ui/core/RadioGroup"
-
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Textfield from "@material-ui/core/Textfield";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import { Link } from "react-router-dom";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+2
 
 export default class CreateRoomPage extends Component {
     constructor(props) {
