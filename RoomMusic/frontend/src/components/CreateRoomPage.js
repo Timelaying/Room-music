34a2c1158069be6a22493de8076df782d46d320a
Timelaying <makanjuola.timi@gmail.com>
2024-11-29 16:53:02 +0000

    stopping project, material ui is bad

--- RoomMusic/frontend/src/components/CreateRoomPage.js
+++ RoomMusic/frontend/src/components/CreateRoomPage.js
@@ -1,38 +1,40 @@
-import React, { Component } from 'react';
-import { Button } from '@mui/material';
-import Grid from '@mui/material/Grid2';
-import { Typography }from '@mui/material';
-import TextField from '@mui/material';
-import { FormHelperText } from '@mui/material';
-import { FormControl } from '@mui/material';
+import React, { Component } from "react";
+import { Button } from "@mui/material";
+import Grid from "@mui/material/Grid2";
+import { Typography } from "@mui/material";
+import TextField from "@mui/material";
+import { FormHelperText } from "@mui/material";
+import { FormControl } from "@mui/material";
 import { Link } from "react-router-dom";
-import Radio from '@mui/material';
-import RadioGroup from '@mui/material';
-import FormControlLabel from '@mui/material';
+import Radio from "@mui/material";
+import RadioGroup from "@mui/material";
+import FormControlLabel from "@mui/material";
 
 export default class CreateRoomPage extends Component {
-    defaultsVotes = 2;
+  defaultsVotes = 2;
 
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render(){
-        // return <Button variant='text'>Text</Button>
-        
-        return <Grid container spacing = {1}>
-            <Grid item xs = {12} align = "center">
-                <Typography Component = 'h4' variant = "h4">
-                    Create a Room
-                </Typography>
-            </Grid>
-            <Grid item xs = {12} align = "center">
-                <FormControl component = "fieldset">
-                    <FormHelperText>
-                        <div align = "center">Guest Control of Playback State</div>
-                    </FormHelperText>
-                </FormControl>
-            </Grid>
-        </Grid>;
-    }
+  render() {
+    // return <Button variant='text'>Text</Button>
+
+    return (
+      <Grid container spacing={1}>
+        <Grid item xs={12} align="center">
+          <Typography Component="h4" variant="h4">
+            Create a Room
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <FormControl component="fieldset">
+            <FormHelperText>
+              <div align="center">Guest Control of Playback State</div>
+            </FormHelperText>
+          </FormControl>
+        </Grid>
+      </Grid>
+    );
+  }
 }
